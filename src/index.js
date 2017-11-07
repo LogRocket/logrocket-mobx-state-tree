d251fc55f614ec212776dfb99a9a7d7c11e65580
chemitaxis <chemitaxis@gmail.com>
2017-11-07 17:38:08 +0100

    Updated to new version and fixed middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { asReduxStore } from "mobx-state-tree/middleware/redux"
+import { asReduxStore } from "mst-middlewares"
 import { addMiddleware } from "mobx-state-tree"
 
 
 export default function logStore(logrocket, mobXStore, options = {}) {
   const reduxMiddleware = logrocket.reduxMiddleware(options);
   const reduxStore = asReduxStore(mobXStore);
-  
+
   const middlewareWithStore = reduxMiddleware(reduxStore);
   return addMiddleware(mobXStore, (call, next) => {
     return middlewareWithStore(next)(call);
